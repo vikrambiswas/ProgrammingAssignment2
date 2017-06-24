eb005fd614d57b550f601cee21e1c1ee35cf3574
vikrambiswas <vikramstl@gmail.com>
2017-06-24 15:08:56 -0500

    Create cacheMatrix.R

---
+++ cacheMatrix.R
@@ -0,0 +1,35 @@
+> makeCacheMatrix <- function(x = matrix()) {
++ i <- NULL
++ set <- function( matrix ) {m <<- matrix
+Error: unexpected end of input
+> makeCacheMatrix <- function(x = matrix()) {
++ i <- NULL
++ set <- function( matrix )
++ {
++ m <<- matrix
++ i <<- NULL
++ }
++ get <- function() {
++ m
++ }
++ setInverse <- function(inverse) {
++ i <<- inverse
++ }
++ getInverse <- function() {
++ i
++ }
++ list(set = set, get = get,
++ setInverse = setInverse,
++ getInverse = getInverse)
++ }
+> cacheSolve <- function(x, ...) {
++ m <- x$getInverse()
++ if( !is.null(m) ) {
++ message("getting cached data")
++ return(m)
++ }
++ data <- x$get()
++ m <- solve(data) %*% data
++ x$setInverse(m)
++ m
++ }
