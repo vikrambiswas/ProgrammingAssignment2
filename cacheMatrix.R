9c4fb0d179b6012537539e7cbc4d9cac8edc495c
vikrambiswas <vikramstl@gmail.com>
2017-06-25 11:04:20 -0500

    Update cacheMatrix.R

--- cacheMatrix.R
+++ cacheMatrix.R
@@ -1,35 +1,35 @@
 > makeCacheMatrix <- function(x = matrix()) {
-+ i <- NULL
-+ set <- function( matrix ) {m <<- matrix
+i <- NULL
+set <- function( matrix ) {m <<- matrix
 Error: unexpected end of input
 > makeCacheMatrix <- function(x = matrix()) {
-+ i <- NULL
-+ set <- function( matrix )
-+ {
-+ m <<- matrix
-+ i <<- NULL
-+ }
-+ get <- function() {
-+ m
-+ }
-+ setInverse <- function(inverse) {
-+ i <<- inverse
-+ }
-+ getInverse <- function() {
-+ i
-+ }
-+ list(set = set, get = get,
-+ setInverse = setInverse,
-+ getInverse = getInverse)
-+ }
+i <- NULL
+set <- function( matrix )
+{
+m <<- matrix
+i <<- NULL
+}
+get <- function() {
+m
+}
+setInverse <- function(inverse) {
+i <<- inverse
+}
+getInverse <- function() {
+i
+}
+list(set = set, get = get,
+setInverse = setInverse,
+getInverse = getInverse)
+}
 > cacheSolve <- function(x, ...) {
-+ m <- x$getInverse()
-+ if( !is.null(m) ) {
-+ message("getting cached data")
-+ return(m)
-+ }
-+ data <- x$get()
-+ m <- solve(data) %*% data
-+ x$setInverse(m)
-+ m
-+ }
+m <- x$getInverse()
+if( !is.null(m) ) {
+message("getting cached data")
+return(m)
+}
+data <- x$get()
+m <- solve(data) %*% data
+x$setInverse(m)
+m
+}
